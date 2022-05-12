d25e31fbebc53229de4a1b01990233418e091950
ivan <ivan_38@comunidad.unam.mx>
2022-05-12 13:45:56 -0500

    conexion catalogo

--- catalogo_discos_desc.php
+++ catalogo_discos_desc.php
@@ -6,29 +6,36 @@ if (isset($_SESSION['valida']) && $_SESSION['valida'] == true){
 
 //Consultar los registros y mostrar los en una tabla
 	include 'conexion.php';
-	$query = "SELECT d.titulo, g.nombre, d.portada FROM discos d inner join grupos g on d.grupo_id = g.grupo_id order by d.disco_id";
+	$titulo= $_GET['titulo'];
+	$query = "SELECT * FROM catalogo_cancion where titulo ='".$titulo."'";
 	$ejecucion = pg_query($con, $query);
-
-//	var_dump($ejecucion);
+	$row = pg_fetch_assoc($ejecucion, 0);
+	//var_dump($ejecucion);
 ?>
+
+	<p><img src="<?php echo $row['portada'];?>" width=100 heigth=100></p>
+	<h3><?php echo $row['titulo'];?></h3>
+	<p><?php echo $row['grupo'];?></p>
 	<table>
 		<tr>
+			<th>#</th>
 			<th>Titulo</th>
-			<th>Grupo</th>
-			<th>Portada</th>
+			<th>compositor</th>
 			<th>Edición</th>
 			<th>Borrado</th>
 		</tr>
 		<?php
+			$cont = 1;
+			$row = pg_result_seek($ejecucion, 0);
 			while($row = pg_fetch_assoc($ejecucion)){
 				echo "<tr>";
-				echo "<td>".$row['titulo']."</td>";
-				echo "<td>".$row['nombre']."</td>";
-				echo "<td><img src=".pg_unescape_bytea($row['portada'])." width=100 heigth=100></td>";
-				//echo "<td><img src=img/darth.jpg width=100 heigth=100></td>";
-				echo "<td><a href='catalogo_discos _desc.php?titulo=".$row['titulo']."'>Editar</a></td>";
+				echo "<td>".$cont."</td>";
+				echo "<td>".$row['cancion']."</td>";
+				echo "<td>".$row['compositor']."</td>";
+				echo "<td><a href='formularioEd.php?id=".$row['id']."'>Editar</a></td>";
 				echo "<td><a href='formularioEl.php?id=".$row['id']."'>Borrar</a></td>";
 				echo "</tr>";
+				$cont++;
 			}
 		?>	
 	</table>
