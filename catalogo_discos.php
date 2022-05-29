47726b557c8fd6ceccb83bbc72f3795fdb9b827f
Diego Puebla <43054925+Diego-PA@users.noreply.github.com>
2022-05-29 15:40:56 -0500

    Estilos catalogo_discos

--- catalogo_discos.php
+++ catalogo_discos.php
@@ -11,29 +11,51 @@ if (isset($_SESSION['valida']) && $_SESSION['valida'] == true){
 
 //	var_dump($ejecucion);
 ?>
-	<table>
-		<tr>
-			<th>Portada</th>
-			<th>Titulo</th>
-			<th>Grupo</th>
-			<th>Edición</th>
-			<th>Borrado</th>
-		</tr>
-		<?php
-			while($row = pg_fetch_assoc($ejecucion)){
-				echo "<tr>";
-				echo "<td><img src=".pg_unescape_bytea($row['portada'])." width=100 heigth=100></td>";
-				//echo "<td><img src=img/darth.jpg width=100 heigth=100></td>";
-				echo "<td>".$row['titulo']."</td>";
-				echo "<td>".$row['nombre']."</td>";
-				echo "<td><a href='catalogo_discos_desc.php?id=".$row['disco_id']."'>Editar</a></td>";
-				echo "<td><a href='formularioEl.php?id=".$row['disco_id']."'>Borrar</a></td>";
-				echo "</tr>";
-			}
-		?>	
-	</table>
-	<input type="submit" onclick="location='salir.php'" value="Salir">
-
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+		<title>Catalogo de Discos</title>
+	</head>
+	<body>
+		<br>
+		<div class="card">
+			<div class="card-header">
+				<h1>Catálogo de discos</h1>
+			</div>
+			<div class="card-body">
+				<table class="table">
+					<tr>
+						<th scope="col">Portada</th>
+						<th scope="col">Titulo</th>
+						<th scope="col">Grupo</th>
+						<th scope="col">Edición</th>
+						<th scope="col">Borrado</th>
+					</tr>
+					<?php
+						while($row = pg_fetch_assoc($ejecucion)){
+							echo "<tr>";
+							echo "<td><img src=".pg_unescape_bytea($row['portada'])." width=100 heigth=100></td>";
+							//echo "<td><img src=img/darth.jpg width=100 heigth=100></td>";
+							echo "<td>".$row['titulo']."</td>";
+							echo "<td>".$row['nombre']."</td>";
+							echo "<td><a class=\"btn btn-outline-secondary\" href='catalogo_discos_desc.php?id=".$row['disco_id']."'>Editar</a></td>";
+							echo "<td><a class=\"btn btn-outline-danger\" href='formularioEl.php?id=".$row['disco_id']."'>Borrar</a></td>";
+							echo "</tr>";
+						}
+					?>	
+				</table>
+			</div>
+		</div>
+		<br>
+		<center>
+			<input class="btn btn-danger" type="submit" onclick="location='salir.php'" value="Salir">
+		</center>
+	</body>
+	</html>
 
 <?php }  	else {
 		header('Location: index.php?error=1');
