bd2c1b054ec65e0cf7adbe53934a18830628a7e0
Diego Puebla <43054925+Diego-PA@users.noreply.github.com>
2022-05-30 00:16:16 -0500

    catalogo compositores

--- catalogo_compositores.php
+++ catalogo_compositores.php
@@ -6,7 +6,7 @@ if (isset($_SESSION['valida']) && $_SESSION['valida'] == true){
 
 //Consultar los registros y mostrar los en una tabla
 	include 'conexion.php';
-	$query = "SELECT d.disco_id, d.titulo, g.nombre, d.portada FROM discos d inner join grupos g on d.grupo_id = g.grupo_id order by d.disco_id";
+	$query = "SELECT c.compositor_id, c.nombre, c.apellido, c.pais_nacimiento, c.fecha_nacimiento FROM compositores c order by c.compositor_id";
 	$ejecucion = pg_query($con, $query);
 
 //	var_dump($ejecucion);
@@ -22,38 +22,42 @@ if (isset($_SESSION['valida']) && $_SESSION['valida'] == true){
 		<title>Catalogo de Compositoes</title>
 	</head>
 		<body>
-			<header class="encabezado">  
-			    <nav>
-			      <a class="nav-a logo" href="catalogo_discos.php"><img class="logo-img" src="logo.png" alt="discos"></a>
-			      <ul>
-			        <li class="menu"><a class="nav-li-link" href="catalogo_discos.php">Inicio</a></li>
-			        <li class="menu"><a href="#">Catalogo</a>
-			          <ul>
-			            <li><a href="catalogo_artistas.php">Artista</a></li>
-			            <li><a href="catalogo_compositores.php">Compositor</a></li>
-			            <li><a href="catalogo_discos.php">Disco</a></li>
-			            <li><a href="catalogo_disqueas.php">Disquera</a></li>
-			            <li><a href="catalogo_grupos.php">Grupo</a></li>
-			            <li><a href="catalogo_poductores.php">Productor</a></li>
-			          </ul>
-			        </li>
-			        <li class="menu"><a href="#">Registro</a>
-			          <ul>
-			            <li><a href="form_artista.php">Artista</a></li>
-			            <li><a href="form_compositor.php">Compositor</a></li>
-			            <li><a href="form_disco.php">Disco</a></li>
-			            <li><a href="form_disquera.php">Disquera</a></li>
-			            <li><a href="form_grupo.php">Grupo</a></li>
-			            <li><a href="form_productor.php">Productor</a></li>
-			          </ul>
-			        </li>
-			        <li class="menu"><a href="creditos.php">Creditos</a></li>
-			        <li class="menu"><a href="salir.php">Salir</a></li>
-			      </ul>
-			    </nav>
-			</header>
-
-		</body>
+		<br>
+		<div class="card text-white bg-dark">
+			<div class="card-header">
+				<h1>Catálogo de Compositores</h1>
+			</div>
+			<div class="card-body">
+				<table class="table">
+					<tr>
+						<th scope="col">#</th>
+						<th scope="col">Nombre</th>
+						<th scope="col">País de Origen</th>
+                        			<th scope="col">Fecha de Nacimiento</th>
+                        			<th scope="col">Editar</th>
+                        			<th scope="col">Borrar</th>
+					</tr>
+					<?php
+                        $nombre = $row['nombre'] . " " . $row['apellido'];
+						while($row = pg_fetch_assoc($ejecucion)){
+							echo "<tr>";
+							echo "<td>".$row['compositor_id']."</td>";
+							echo "<td>".$nombre."</td>";
+							echo "<td>".$row['pais_nacimiento']."</td>";
+                            				echo "<td>".$row['fecha_nacimiento']."</td>";
+							echo "<td><a class=\"btn btn-outline-secondary\" href='catalogo_discos_desc.php?id=".$row['grupo_id']."'>Editar</a></td>";
+							echo "<td><a class=\"btn btn-outline-danger\" href='formularioEl.php?id=".$row['grupo_id']."'>Borrar</a></td>";
+							echo "</tr>";
+						}
+					?>	
+				</table>
+			</div>
+		</div>
+		<br>
+		<center>
+			<input class="btn btn-danger" type="submit" onclick="location='salir.php'" value="Salir">
+		</center>
+	</body>
 	</html>
 
 <?php }  	else {
